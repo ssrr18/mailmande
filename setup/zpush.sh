e5f42a665c558fb7b7414b42b4d04aaf50d0ab0b
ssrr18 <41035150+ssrr18@users.noreply.github.com>
2018-07-10 10:06:57 +0600

    fdsdg

--- setup/zpush.sh
+++ setup/zpush.sh
@@ -10,23 +10,3 @@
 #
 # Thanks to http://frontender.ch/publikationen/push-mail-server-using-nginx-and-z-push.html.
 
-source setup/functions.sh # load our functions
-source /etc/mailinabox.conf # load global vars
-
-# Prereqs.
-
-echo "Installing Z-Push (Exchange/ActiveSync server)..."
-apt_install \
-	php7.0-soap php7.0-imap libawl-php php7.0-xsl
-
-phpenmod -v php7.0 imap
-
-# Copy Z-Push into place.
-
-# Restart service.
-
-restart_service php7.0-fpm
-
-# Fix states after upgrade
-
-hide_output z-push-admin -a fixstates
